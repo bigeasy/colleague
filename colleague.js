dd5cecda4449521a64b1d5371fba4e683406fd4a
Alan Gutierrez <alan@prettyrobots.com>
2017-04-12 13:32:26 -0500

    Upgrade dependencies.

--- colleague.js
+++ colleague.js
@@ -5,23 +5,26 @@ var getPipe = require('./pipe')(require('net'))
 var Conduit = require('conduit')
 var Procession = require('procession')
 var Destructible = require('destructible')
+var Signal = require('signal')
 
 function Colleague (conference) {
     this._destructible = new Destructible
     this._conference = conference
     this.demolition = this._destructible.events
+    this.ready = new Signal
 }
 
 Colleague.prototype.listen = cadence(function (async, process) {
     var pipe = getPipe(process, coalesce(process.env['COMPASSION_COLLEAGUE_FD'], 'stdin/stdout'))
     var conduit = new Conduit(pipe.input, pipe.output)
-    conduit.read.pump(this._conference.write)
-    this._conference.read.pump(conduit.write)
-    this._destructible.async(async, 'conduit')(function (ready) {
+    conduit.read.pump(this._conference.write, 'enqueue')
+    this._conference.read.pump(conduit.write, 'enqueue')
+    this._destructible.stack(async, 'conduit')(function (ready) {
         this._destructible.addDestructor('conduit', conduit.destroy.bind(conduit))
         conduit.listen(async())
         ready.unlatch()
     })
+    this._destructible.ready.wait(this.ready, 'unlatch')
 })
 
 Colleague.prototype.destroy = function (callback) {
